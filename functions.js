9e873bd7b3b4e6cc4f8a7637227221f214c24505
Lautaro Cheppi <cheppimdp@gmail.com>
2023-02-13 00:10:35 -0300

    vamos

---
+++ functions.js
@@ -0,0 +1,10 @@
+// comentario de una sola lines
+
+/**
+ * comentario
+ * de muchas
+ * lineas
+ */
+
+//se puede declarar una variable con cualquiera -->> utilizar nombres unicos y descriptivos, discimina mayusculas y minusculas 
+
